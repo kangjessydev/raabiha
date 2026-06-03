b84438e94af1774677a2d5892428386fc6b3b545
Jessy Kang <kangjessy.dev@gmail.com>
2026-06-03 16:28:25 +0700

    Fix TopbarAnnouncement blade view components

--- resources/views/filament/pages/topbar-announcement.blade.php
+++ resources/views/filament/pages/topbar-announcement.blade.php
@@ -1,9 +1,11 @@
 <x-filament-panels::page>
-    <x-filament-panels::form wire:submit="save">
+    <form wire:submit="save" class="space-y-6">
         {{ $this->form }}
 
-        <x-filament-panels::form.actions
-            :actions="$this->getFormActions()"
-        />
-    </x-filament-panels::form>
+        <div class="mt-4 flex gap-4">
+            <x-filament::button type="submit" wire:target="save">
+                Simpan Pengaturan
+            </x-filament::button>
+        </div>
+    </form>
 </x-filament-panels::page>
