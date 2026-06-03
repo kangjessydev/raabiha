385b87cd78b136adb3ad546f37b9d6342a19a79d
Jessy Kang <kangjessy.dev@gmail.com>
2026-06-03 15:14:34 +0700

    Fix MediaResource navigationGroup type hint mismatch

--- app/Filament/Resources/MediaResource.php
+++ app/Filament/Resources/MediaResource.php
@@ -9,9 +9,6 @@ class MediaResource extends CuratorMediaResource
 {
     protected static ?string $cluster = MediaFiles::class;
     
-    // We remove the default group so it uses the cluster
-    protected static ?string $navigationGroup = null;
-    
     // Maintain the icon
     protected static ?string $navigationIcon = 'heroicon-o-photo';
 }
