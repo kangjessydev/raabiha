a9d32a6a040dd308cf05d7764eaeed0ffda52fb8
Jessy Kang <kangjessy.dev@gmail.com>
2026-06-06 20:32:30 +0700

    fix(admin): add getSubNavigation to PromoBanner pages to show cluster sidebar

--- app/Filament/Resources/PromoBanners/Pages/CreatePromoBanner.php
+++ app/Filament/Resources/PromoBanners/Pages/CreatePromoBanner.php
@@ -8,4 +8,13 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePromoBanner extends CreateRecord
 {
     protected static string $resource = PromoBannerResource::class;
+
+    public function getSubNavigation(): array
+    {
+        if (filled($cluster = static::getCluster()) && $cluster::shouldRegisterSubNavigation()) {
+            return $this->generateNavigationItems($cluster::getClusteredComponents());
+        }
+
+        return [];
+    }
 }
