2870f3153f01f00c223488c37498ae5e63f1f185
Jessy Kang <kangjessy.dev@gmail.com>
2026-07-11 13:30:07 +0700

    fix: make view property non-static in PaymentMethodsNotice widget to resolve PHP fatal error

--- app/Filament/Widgets/PaymentMethodsNotice.php
+++ app/Filament/Widgets/PaymentMethodsNotice.php
@@ -6,5 +6,5 @@ use Filament\Widgets\Widget;
 
 class PaymentMethodsNotice extends Widget
 {
-    protected static string $view = 'filament.widgets.payment-methods-notice';
+    protected string $view = 'filament.widgets.payment-methods-notice';
 }
